1b86fd657d1d01824da6f41bb90536fb0932ad98
ysolarh <ysolarh@gmail.com>
2023-12-21 18:16:24 +0900

    231221 update js_1

---
+++ 1_231221/04_review/04-2_carousel/231221_04-2_ carousel.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body');
+const carousel = document.querySelector('.carousel');
+
+function darkmode() {
+    body.classList.toggle("active")
+}
+
+
+
+function clickPrev() {
+
+}
+
+function clickNext() {
+
+}
+
+function clickButton(number) {
+    
+}
