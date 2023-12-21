772e8ead920b353d083b9ada9f64a8e5274c8816
ysolarh <ysolarh@gmail.com>
2023-12-21 18:56:50 +0900

    231221 update js_1-4-2

--- 1_231221/04_review/04-2_carousel/231221_04-2_ carousel.js
+++ 1_231221/04_review/04-2_carousel/231221_04-2_ carousel.js
@@ -1,20 +1,39 @@
 const body = document.querySelector('body');
 const carousel = document.querySelector('.carousel');
+const num = document.querySelector('.number');
+
+let idx = 0;
+const maxIdx = 2;
 
 function darkmode() {
-    body.classList.toggle("active")
+    body.classList.toggle("active");
+    num.classList.toggle("active");
 }
 
-
+function switchImage(idx) {
+    carousel.style.transform = `translate3d(-${500 * idx}px, 0, 0)`
+}
 
 function clickPrev() {
-
+    if (idx === 0) {
+        idx = maxIdx;
+    }
+    else {
+        idx -= 1;
+    }
+    switchImage(idx);
 }
 
 function clickNext() {
-
+    if (idx === maxIdx) {
+        idx = 0;
+    }
+    else {
+        idx += 1;
+    }
+    switchImage(idx)
 }
 
 function clickButton(number) {
-    
+    switchImage(number - 1)
 }
