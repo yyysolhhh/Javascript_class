7a3e0119edf29bd8b9612a7f308d19ab011e14ae
ysolarh <ysolarh@gmail.com>
2023-12-23 16:24:14 +0900

    231223 update js_2-3-4

--- 2_231222/03_review/03-4_mystery_number/231222_03-4_mystery_number.js
+++ 2_231222/03_review/03-4_mystery_number/231222_03-4_mystery_number.js
@@ -5,26 +5,33 @@ const randNum = Math.floor(Math.random() * 100 + 1);
 
 function clickBtn() {
   const inputNum = document.querySelector(".input__num").value;
-  // inputNum = parseInt(inputNum.value);
+  inputIntNum = parseInt(inputNum);
 
-  if (inputNum == randNum) {
-    alert("정답!");
-    retryBtn.style.display = "block";
+  if (inputIntNum > 100 || inputIntNum < 0) {
     hint[0].style.display = "none";
     hint[1].style.display = "none";
-  } else if (inputNum < randNum) {
-    hint[0].style.display = "block";
-    hint[1].style.display = "none";
+    hint[2].style.display = "block";
   } else {
-    hint[1].style.display = "block";
-    hint[0].style.display = "none";
+    if (inputIntNum === randNum) {
+      alert("정답!");
+      retryBtn.style.display = "block";
+      hint[0].style.display = "none";
+      hint[1].style.display = "none";
+      hint[2].style.display = "none";
+    } else if (inputIntNum < randNum) {
+      hint[0].style.display = "block";
+      hint[1].style.display = "none";
+      hint[2].style.display = "none";
+    } else {
+      hint[0].style.display = "none";
+      hint[1].style.display = "block";
+      hint[2].style.display = "none";
+    }
   }
   return false;
 }
 
 function clickReset() {
   location.reload();
-  hint[0].style.display = "none";
-  hint[1].style.display = "none";
   return false;
 }
