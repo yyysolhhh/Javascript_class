be480de343d6841beecc41b1660db00010a6415d
ysolarh <ysolarh@gmail.com>
2023-12-27 02:24:08 +0900

    231227 retry js3-4-1

--- 3_231226/04_review/04-1_calculator/231226_04-1_calculator.js
+++ 3_231226/04_review/04-1_calculator/231226_04-1_calculator.js
@@ -1,27 +1,11 @@
 const form = document.getElementById("form");
 
-function add(n1, n2) {
-  return n1 + n2;
-}
-
-function subtract(n1, n2) {
-  return n1 - n2;
-}
-
-function multiply(n1, n2) {
-  return n1 * n2;
-}
-
-function divide(n1, n2) {
-  return n1 / n2;
-}
-
 function clear() {
   location.reload();
 }
 
-form.addEventListener("click");
+// form.addEventListener("click", handleClick);
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+// });
