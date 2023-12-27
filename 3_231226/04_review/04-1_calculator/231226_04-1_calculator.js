4c7f8fc739a2c74f02ffb260883a9620b0051d19
ysolarh <ysolarh@gmail.com>
2023-12-28 02:19:23 +0900

    231228 update js3-4-1 동적함수

--- 3_231226/04_review/04-1_calculator/231226_04-1_calculator.js
+++ 3_231226/04_review/04-1_calculator/231226_04-1_calculator.js
@@ -1,67 +1,23 @@
-const form = document.getElementById("form");
 const result = document.querySelector(".result");
-let inputValue = "";
-let currIdx = 0;
 let restart = false;
-const nums = [];
-const operators = [];
+let buffer = "";
 
 function inputClear() {
-  // location.reload(true);
-  inputValue = "";
-  currIdx = 0;
-  nums.splice(0);
-  operators.splice(0);
+  buffer = "";
   result.value = "";
 }
 
-function inputNum(strNum) {
+function input(strValue) {
   if (restart) {
     inputClear();
     restart = false;
   }
-  inputValue += strNum;
-  result.value += strNum;
-  nums[currIdx] = "";
-  nums[currIdx] += strNum;
-  // console.log(strNum);
-  // console.log(operators);
-}
-
-function inputOp(op) {
-  if (restart) {
-    inputClear();
-    restart = false;
-  }
-  inputValue += op;
-  result.value += op;
-  operators.push(op);
-  currIdx += 1;
-  console.log(nums);
-}
-
-function calculate(n1, op, n2) {
-  n1 = parseInt(n1);
-  n2 = parseInt(n2);
-  switch (op) {
-    case "+":
-      return n1 + n2;
-    case "-":
-      return n1 - n2;
-    case "/":
-      return n1 / n2;
-    case "*":
-      return n1 * n2;
-  }
+  buffer += strValue;
+  result.value += strValue;
 }
 
 function inputEq() {
-  let res = nums.shift();
-  console.log(res);
-  while (nums[0] && operators[0]) {
-    res = calculate(res, operators.shift(), nums.shift());
-    console.log(res);
-  }
-  result.value = res;
+  let func = new Function("return " + buffer);
+  result.value = func();
   restart = true;
 }
