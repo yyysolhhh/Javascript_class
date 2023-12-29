6a6179a0f13e13902f5f9bb15f05a701c559ecd2
ysolarh <ysolarh@gmail.com>
2023-12-29 17:43:13 +0900

    design: js5-3 update css black and white concept

--- 5_231228/03_todo_list/231228_03_todo_list.js
+++ 5_231228/03_todo_list/231228_03_todo_list.js
@@ -2,4 +2,33 @@ const todoList = document.getElementById("todo-list");
 const todoForm = document.getElementById("todo-form");
 const todoArr = [];
 
-todoForm.addEventListener("submit", (e) => {});
+// 할일 삭제
+
+// 할일 완료
+
+// 할일 출력
+function displayTodos() {
+  todoList.textContent = "";
+  todoArr.forEach((item) => {
+    const todoItem = document.createElement("li");
+    const todoCheck = document.createElement("input");
+    todoCheck.setAttribute("type", "checkbox");
+    todoItem.textContent = item.todoText;
+    todoList.appendChild(todoItem);
+    todoItem.prepend(todoCheck);
+    console.log(todoItem);
+  });
+}
+
+// 할일 추가
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const toBeAdded = {
+    todoText: todoForm.todo.value,
+    todoId: new Date().getTime(),
+    todoDone: false,
+  };
+  todoForm.todo.value = "";
+  todoArr.push(toBeAdded);
+  displayTodos();
+});
