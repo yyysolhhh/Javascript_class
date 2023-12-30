f6603ba2d25f8e1a5ca00c4770588d96db298110
ysolarh <ysolarh@gmail.com>
2023-12-30 18:02:35 +0900

    js5-4 complete

--- 5_231228/04_review/231228_04_report_card.js
+++ 5_231228/04_review/231228_04_report_card.js
@@ -1,22 +1,14 @@
 const reportTable = document.getElementById("report");
 const highScoreStH2 = document.getElementById("high-score");
-
-// console.log(reportTable.rows[1].cells[0].textContent);
 const tableValue = Array.from(reportTable.rows).slice(1);
-console.log(tableValue);
-let scores = {};
-let highScoreStudents = [];
+
+const scores = {};
+const highScoreStudents = [];
 
 // tableValue -> scores
 tableValue.forEach((item) => {
   scores[item.cells[0].innerHTML] = parseInt(item.cells[1].innerHTML);
 });
-console.log("scores", scores);
-
-// Array.from(scores).sort((a, b) => {
-//   return b[1] - a[1];
-// });
-// console.log(scores);
 
 // 75점 이상 추출
 for (const name in scores) {
@@ -25,13 +17,10 @@ for (const name in scores) {
   }
 }
 
-// highScoreStudents = Array.from(scores).filter((item) => item[1] >= 75);
-console.log(scores);
 // 점수 높은 순으로 나열
 highScoreStudents.sort((a, b) => {
   return b[1] - a[1];
 });
-console.log(highScoreStudents);
 
 // 화면에 출력
 for (const name of highScoreStudents) {
