48cfd1a342a6658c81482560f4deeaf7898f4b54
ysolarh <ysolarh@gmail.com>
2024-01-01 03:59:55 +0900

    js5-6 setinterval 두개 동시에 작동하는 문제 해결

--- 5_231228/06_countdown_timer/231228_06_countdown_timer.js
+++ 5_231228/06_countdown_timer/231228_06_countdown_timer.js
@@ -6,50 +6,49 @@ const secDiv = document.getElementById("seconds");
 const startBtn = document.querySelector(".start-btn");
 const setBtn = document.querySelector(".set-btn");
 
+// let days, hours, mins, secs;
+
 function countdown(yy, mm, dd, hh) {
   const now = new Date();
   // const dday = Date.parse(`${yy}/${mm}/${dd} ${hh}:00:00`);
   // const dday = new Date(yy, mm, dd, hh, 0, 0);
-  const dday = new Date(yy, mm, dd);
+  const dday = new Date(yy, mm, dd, hh);
   // console.log(now);
   // console.log(dday);
   const diff = dday - now;
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(diff / (1000 * 60 * 60));
-  const mins = Math.floor(diff / (1000 * 60));
-  const secs = Math.floor(diff / 1000);
-  const h = hours - days * 24;
-  const m = mins - hours * 60;
-  const s = secs - mins * 60;
+  const h = Math.floor(diff / (1000 * 60 * 60));
+  const m = Math.floor(diff / (1000 * 60));
+  const s = Math.floor(diff / 1000);
+  const hours = h - days * 24;
+  const mins = m - h * 60;
+  const secs = s - m * 60;
 
   // currTime.innerText = `현재 날짜와 시간: ${now}`;
   dayDiv.innerText = String(days).padStart(2, "0");
-  hourDiv.innerText = String(h).padStart(2, "0");
-  minDiv.innerText = String(m).padStart(2, "0");
-  secDiv.innerText = String(s).padStart(2, "0");
+  hourDiv.innerText = String(hours).padStart(2, "0");
+  minDiv.innerText = String(mins).padStart(2, "0");
+  secDiv.innerText = String(secs).padStart(2, "0");
 
-  // console.log(days, h, m, s);
+  // console.log(days, hours, mins, secs);
 
-  if (diff <= 0) {
+  if (diff <= 1) {
     clearInterval(intervalId);
   }
 }
 
-// setBtn.addEventListener("click", () => {
-//   const y = prompt("년");
-//   const m = prompt("월");
-//   const d = prompt("일");
-//   const h = prompt("시");
-
-//   countdown(y, m, d, h);
-// });
-
 let intervalId;
+// let intervalId2;
 startBtn.addEventListener("click", () => {
+  alert("D-day를 입력해주세요.");
   const y = prompt("년");
   const m = prompt("월");
   const d = prompt("일");
-  // const h = prompt("시");
+  const h = prompt("시");
 
-  intervalId = setInterval(countdown, 1000, y, m - 1, d);
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  intervalId = setInterval(countdown, 1000, y, m - 1, d, h);
+  // intervalId2 = setInterval(fillTimer, 1000);
 });
