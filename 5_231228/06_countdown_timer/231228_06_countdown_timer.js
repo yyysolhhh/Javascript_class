d57c1bc712ba688974db22b2033810cd41171a26
ysolarh <ysolarh@gmail.com>
2024-01-01 16:17:00 +0900

    js5-6 animation 추가

--- 5_231228/06_countdown_timer/231228_06_countdown_timer.js
+++ 5_231228/06_countdown_timer/231228_06_countdown_timer.js
@@ -6,7 +6,7 @@ const secDiv = document.getElementById("seconds");
 const startBtn = document.querySelector(".start-btn");
 const setBtn = document.querySelector(".set-btn");
 
-// let days, hours, mins, secs;
+let days, hours, mins, secs;
 
 function countdown(yy, mm, dd, hh) {
   const now = new Date();
@@ -16,13 +16,13 @@ function countdown(yy, mm, dd, hh) {
   // console.log(now);
   // console.log(dday);
   const diff = dday - now;
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const h = Math.floor(diff / (1000 * 60 * 60));
   const m = Math.floor(diff / (1000 * 60));
   const s = Math.floor(diff / 1000);
-  const hours = h - days * 24;
-  const mins = m - h * 60;
-  const secs = s - m * 60;
+  hours = h - days * 24;
+  mins = m - h * 60;
+  secs = s - m * 60;
 
   dayDiv.innerText = String(days).padStart(2, "0");
   hourDiv.innerText = String(hours).padStart(2, "0");
@@ -30,10 +30,35 @@ function countdown(yy, mm, dd, hh) {
   secDiv.innerText = String(secs).padStart(2, "0");
 
   // console.log(days, hours, mins, secs);
+  // animationStart();
 
   if (diff <= 1) {
     clearInterval(intervalId);
+    animationState("paused");
   }
+}
+
+// function animationStart() {
+//   console.log(1);
+//   secDiv.animate([{ top: 0 }, { bottom: "100%" }], {
+//     duration: 1000,
+//     easing: "ease-in-out",
+//     direction: "alternate",
+//     iteration: "infinite",
+//   });
+//   minDiv.animate([{ top: 0 }, { bottom: "100%" }], {
+//     duration: 1000,
+//     easing: "ease-in-out",
+//     direction: "alternate",
+//     iteration: "infinite",
+//   });
+// }
+
+function animationState(state) {
+  secDiv.style.animationPlayState = state;
+  minDiv.style.animationPlayState = state;
+  hourDiv.style.animationPlayState = state;
+  dayDiv.style.animationPlayState = state;
 }
 
 let intervalId;
@@ -49,4 +74,11 @@ startBtn.addEventListener("click", () => {
     clearInterval(intervalId);
   }
   intervalId = setInterval(countdown, 1000, y, m - 1, d, h);
+  minDiv.style.top = 500 + "px";
+  console.log(secs);
+  // minDiv.style.top = (secs / 60) * 100 + "%";
+  // hourDiv.style.top = `(${mins} / 60 * 100)%`;
+  // dayDiv.style.top = `(${hours} / 24 * 100)%`;
+  animationState("running");
+  // animationStart();
 });
